093dc5b9f63fb25736a37faa6fc6272d60742424
zhangpeng <peng.zhang@aiads.com>
2018-11-30 16:24:17 +0800

    修复不能编译的问题

---
+++ tools/fixiview.mjs
@@ -0,0 +1,12 @@
+/**
+ * 临时解决方案，修复 iview types 中的一个小错误
+ */
+
+import fs from 'fs'
+
+const fpath = 'node_modules/iview/dist/types/iview.components.d.ts'
+
+const text = fs.readFileSync(fpath, { encoding: 'utf8' })
+const newText = text.replace('TabsPane', 'TabPane')
+
+fs.writeFileSync(fpath, newText)
