528e205d4d0c9b7a8a10cb0045ca81ce37510b28
zhangpeng <peng.zhang@aiads.com>
2018-12-11 00:40:44 +0800

    完成登录

--- src/routers.ts
+++ src/routers.ts
@@ -1,101 +1,103 @@
 /**
- * !! 警告：该文件使用 yarn uproute 自动生成，不要手动维护 !!
+ * 该文件改为手动维护，但请注意，相似的路径写在一起
  */
-
 export default [
   {
     path: '/about',
     name: 'about',
-    component: () => import(/* webpackChunkName: "about" */ './views/about.vue')
+    component: () => import('./views/about.vue')
+  },
+
+  {
+    path: '/client/account/',
+    name: 'client-account',
+    component: () => import('./views/client/account/index.vue')
   },
   {
     path: '/client/account/detail/:id?',
     name: 'client-account-detail',
-    component: () => import(/* webpackChunkName: "client-account-detail" */ './views/client/account/detail.vue')
-  },
-  {
-    path: '/client/account/',
-    name: 'client-account',
-    component: () => import(/* webpackChunkName: "client-account" */ './views/client/account/index.vue')
+    component: () => import('./views/client/account/detail.vue')
   },
   {
     path: '/client/account/viewLog',
     name: 'client-account-viewLog',
-    component: () => import(/* webpackChunkName: "client-account-viewLog" */ './views/client/account/viewLog.vue')
+    component: () => import('./views/client/account/viewLog.vue')
   },
-  {
-    path: '/client/corp/detail/:id?',
-    name: 'client-corp-detail',
-    component: () => import(/* webpackChunkName: "client-corp-detail" */ './views/client/corp/detail.vue')
-  },
-  {
-    path: '/client/corp/edit/:id?',
-    name: 'client-corp-edit',
-    component: () => import(/* webpackChunkName: "client-corp-edit" */ './views/client/corp/edit.vue')
-  },
+
   {
     path: '/client/corp/',
     name: 'client-corp',
     component: () => import(/* webpackChunkName: "client-corp" */ './views/client/corp/index.vue')
   },
   {
+    path: '/client/corp/detail/:id?',
+    name: 'client-corp-detail',
+    component: () => import('./views/client/corp/detail.vue')
+  },
+  {
+    path: '/client/corp/edit/:id?',
+    name: 'client-corp-edit',
+    component: () => import('./views/client/corp/edit.vue')
+  },
+
+  {
     path: '/client/verify/',
     name: 'client-verify',
-    component: () => import(/* webpackChunkName: "client-verify" */ './views/client/verify/index.vue')
+    component: () => import('./views/client/verify/index.vue')
   },
   {
     path: '/data/area/',
     name: 'data-area',
-    component: () => import(/* webpackChunkName: "data-area" */ './views/data/area/index.vue')
+    component: () => import('./views/data/area/index.vue')
   },
   {
     path: '/data/calendar/',
     name: 'data-calendar',
-    component: () => import(/* webpackChunkName: "data-calendar" */ './views/data/calendar/index.vue')
+    component: () => import('./views/data/calendar/index.vue')
   },
-  {
-    path: '/data/cinema/',
-    name: 'data-cinema',
-    component: () => import(/* webpackChunkName: "data-cinema" */ './views/data/cinema/index.vue')
-  },
+
   {
     path: '/data/cinema-chain/',
     name: 'data-cinema-chain',
-    component: () => import(/* webpackChunkName: "data-cinema-chain" */ './views/data/cinema-chain/index.vue')
+    component: () => import('./views/data/cinema-chain/index.vue')
+  },
+
+  {
+    path: '/data/cinema/',
+    name: 'data-cinema',
+    component: () => import('./views/data/cinema/index.vue')
   },
   {
     path: '/data/cinema-hall/:id',
     name: 'data-cinema-hall',
-    component: () => import(/* webpackChunkName: "data-cinema-hall" */ './views/data/cinema-hall/index.vue')
+    component: () => import('./views/data/cinema-hall/index.vue')
   },
+
   {
     path: '/data/dict/',
     name: 'data-dict',
-    component: () => import(/* webpackChunkName: "data-dict" */ './views/data/dict/index.vue')
+    component: () => import('./views/data/dict/index.vue')
   },
   {
     path: '/data/dict/viewDictionary/:id?',
     name: 'data-dict-viewDictionary',
-    component: () => import(/* webpackChunkName: "data-dict-viewDictionary" */ './views/data/dict/viewDictionary.vue')
+    component: () => import('./views/data/dict/viewDictionary.vue')
   },
+
   {
-    path: '/data/dict-category/',
-    name: 'data-dict-category',
-    component: () => import(/* webpackChunkName: "data-dict-category" */ './views/data/dict-category/index.vue')
+    path: '/data/film/',
+    name: 'data-film',
+    component: () => import('./views/data/film/index.vue')
   },
   {
     path: '/data/film/detail/:id',
     name: 'data-film-detail',
-    component: () => import(/* webpackChunkName: "data-film-detail" */ './views/data/film/detail.vue')
+    component: () => import('./views/data/film/detail.vue')
   },
+
   {
-    path: '/data/film/',
-    name: 'data-film',
-    component: () => import(/* webpackChunkName: "data-film" */ './views/data/film/index.vue')
-  },
-  {
-    path: '/data/film/inputEdit',
-    name: 'data-film-inputEdit',
-    component: () => import(/* webpackChunkName: "data-film-inputEdit" */ './views/data/film/inputEdit.vue')
+    path: '/data/dict-category/',
+    name: 'data-dict-category',
+    component: () => import('./views/data/dict-category/index.vue')
   }
 ] as any[]
---
+++ src/util/tryParseJson.ts
@@ -0,0 +1,12 @@
+/**
+ * 尝试解析 JSON，解析不成功，返回默认值
+ * @param json JSON 字符串
+ * @param defaultValue 默认值
+ */
+export default function(json: string, defaultValue = null) {
+  try {
+    return JSON.parse(json)
+  } catch (ex) {
+    return defaultValue
+  }
+}
