1e953e05df5b1f747f28ef224dd4442d97616add
zhangpeng <peng.zhang@aiads.com>
2018-12-17 16:09:18 +0800

    上传例子

--- src/views/about.vue
+++ src/views/about.vue
@@ -20,6 +20,33 @@ export default class Main extends View {
         keepTypes: [ ImageType.gif, ImageType.png ]
       }
     } as UploaderOptions)
+    uploader.on({
+      thumb: ({ thumb }) => {
+        // 已经生成预览图的时候触发，只有上传的是图片的时候，才有这个事件
+        // thumb 是图片预览地址，可以直接绑定到 img 的 src 上实现预览
+      },
+      begin: () => {
+        // 开始上传
+      },
+      progress: (e: ProgressEvent) {
+        const { loaded, total } = e
+        const percent = Math.floor(loaded * 100 / total)
+        // loaded 是已上传的字节数，total 是总字节数，percent 是计算出来的百分比
+      },
+      done: data => {
+        const url = data.url
+        const fileId = data.fileId
+        // 上传成功，url 是上传后的地址，图片预览需要这个值
+        // fileId 是文件 ID，一般后端需要这个值
+      },
+      fail: ex => {
+        // ex 是与 ajax 组件相同的错误对象
+        this.handleError(ex)
+      },
+      end: () => {
+        // 结束上传，无论成功还是失败，都会执行
+      }
+    })
     uploader.upload(files[0])
   }
 }
