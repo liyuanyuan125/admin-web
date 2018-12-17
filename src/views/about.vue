12aa8bb194f84acc9fde4f18a83ce489c92ac968
zhangpeng <peng.zhang@aiads.com>
2018-12-17 16:10:27 +0800

    完善举例注释

--- src/views/about.vue
+++ src/views/about.vue
@@ -26,7 +26,7 @@ export default class Main extends View {
         // thumb 是图片预览地址，可以直接绑定到 img 的 src 上实现预览
       },
       begin: () => {
-        // 开始上传
+        // 开始上传，执行开始动作，比如显示 loading 之类的
       },
       progress: (e: ProgressEvent) {
         const { loaded, total } = e
@@ -44,7 +44,7 @@ export default class Main extends View {
         this.handleError(ex)
       },
       end: () => {
-        // 结束上传，无论成功还是失败，都会执行
+        // 结束上传，无论成功还是失败，都会执行，执行清理工作，比如取消显示 loading
       }
     })
     uploader.upload(files[0])
