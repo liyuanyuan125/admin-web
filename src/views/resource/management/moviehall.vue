bce993feef793667702437621dfed11151d7fae9
陈鹏宇 <pengyu.chen@aiad.com>
2019-01-22 16:14:05 +0800

    刊例价管理 添加影厅

---
+++ src/views/resource/management/moviehall.vue
@@ -0,0 +1,24 @@
+<template>
+  <Modal 
+    v-model='showDlg'
+    :transfer='false'
+    :width='620'
+    title='关联影厅'
+    @on-cancel="cancel"
+    >
+    <div  slot="footer" class="dialog-footer">
+    </div>
+  </Modal>
+</template>
+
+<script lang="ts">
+import { Component } from 'vue-property-decorator'
+import ViewBase from '@/util/ViewBase'
+
+@Component
+export default class Main extends ViewBase {
+}
+</script>
+
+<style lang="less" scoped>
+</style>
