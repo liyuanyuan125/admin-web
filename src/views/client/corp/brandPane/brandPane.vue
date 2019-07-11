be667a0bbb72d554fbf24bf15615b102c63f5763
zhangpeng <peng.zhang@aiads.com>
2019-07-11 13:19:21 +0800

    抽象出 remoteSelect

--- src/views/client/corp/brandPane/brandPane.vue
+++ src/views/client/corp/brandPane/brandPane.vue
@@ -1,22 +1,12 @@
 <template>
   <section class="brand-pane">
     <div class="action-bar">
-      <Select
+      <RemoteSelect
         v-model="id"
-        filterable
-        clearable
-        remote
-        :remote-method="fetch"
-        :loading="loading"
+        :fetch="searchBrandByName"
         class="brand-select"
         placeholder="请输入品牌名"
-      >
-        <Option
-          v-for="(it, i) in selectList"
-          :key="i"
-          :value="it.id"
-        >{{it.name}}</Option>
-      </Select>
+      />
 
       <Button
         type="primary"
@@ -53,8 +43,13 @@ import { isEqual } from 'lodash'
 import { searchBrandByName } from './data'
 import { Brand, BrandBind } from './types'
 import { alert } from '@/ui/modal'
+import RemoteSelect from '@/components/remoteSelect'
 
-@Component
+@Component({
+  components: {
+    RemoteSelect
+  }
+})
 export default class BrandPane extends ViewBase {
   @Prop({ type: Array, default: () => [] }) value!: number[]
 
@@ -64,8 +59,6 @@ export default class BrandPane extends ViewBase {
   model = this.value
 
   id = 0
-
-  loading = false
 
   selectList: Brand[] = []
 
@@ -81,21 +74,10 @@ export default class BrandPane extends ViewBase {
     ]
   }
 
-  async fetch(query: string) {
-    const name = query.trim()
-    if (name) {
-      this.loading = true
-      try {
-        const list = await searchBrandByName(name)
-        this.selectList = list
-      } catch (ex) {
-        this.handleError(ex)
-      } finally {
-        this.loading = false
-      }
-    } else {
-      this.list = []
-    }
+  async searchBrandByName(query: string) {
+    const list = await searchBrandByName(query)
+    this.selectList = list
+    return list
   }
 
   addBrand() {
