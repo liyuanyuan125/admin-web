36ed44f727f8be8a4472ebc8589d72bf8feb5dfc
zhangpeng <peng.zhang@aiads.com>
2019-07-19 20:18:47 +0800

    下刊触发

---
+++ src/views/system/issue/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="page">
+    <UploadButton
+      buttonType="primary"
+      :uploaderOptions="options"
+      @success="uploadSuccess"
+    >上传 Excel</UploadButton>
+
+    <Card title="文件信息" class="file-card" v-if="file != null">
+      <Cell title="fileId" :extra="file.fileId"/>
+      <Cell title="url" :extra="file.url"/>
+      <Cell title="clientName" :extra="file.clientName"/>
+      <Cell title="clientSize" :extra="file.clientSize"/>
+      <Cell title="clientType" :extra="file.clientType"/>
+    </Card>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component } from 'vue-property-decorator'
+import ViewBase from '@/util/ViewBase'
+import UploadButton, { FileItem } from '@/components/UploadButton.vue'
+import { UploaderOptions } from '@/util/Uploader'
+
+@Component({
+  components: {
+    UploadButton
+  }
+})
+export default class IssueIndex extends ViewBase {
+  file: FileItem | null = null
+
+  options: UploaderOptions = {
+    filePostUrl: '/file/miscs',
+  }
+
+  uploadSuccess({ files }: { files: FileItem[] }) {
+    this.file = files && files[0]
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.file-card {
+  margin-top: 15px;
+  width: 600px;
+}
+</style>
