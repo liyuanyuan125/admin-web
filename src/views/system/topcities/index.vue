7121c5d0ab3a6e6c7f5c18fea55503e0e4a3c019
聂晓东 <xiaodong.nie@aiads.com>
2019-07-25 16:46:41 +0800

    page init

---
+++ src/views/system/topcities/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="page">
+    票房城市TOP20设置 
+  </div>
+</template>
+
+<script lang="ts">
+import { Component } from 'vue-property-decorator'
+import ViewBase from '@/util/ViewBase'
+
+@Component({
+  components: {
+  }
+})
+export default class TopCities extends ViewBase {
+
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
