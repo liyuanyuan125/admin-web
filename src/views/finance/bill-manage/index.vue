a1e2dc13be1b29455259f25d74bf89f59fc6c8f6
李园园 <yuanyuan.li@aiads.com>
2019-08-30 10:50:09 +0800

    -m

--- src/views/finance/bill-manage/index.vue
+++ src/views/finance/bill-manage/index.vue
@@ -15,10 +15,10 @@
         <div class="operate-btn">
           <span @click="$router.push({name: 'finance-billmanage-detail', params: {id}})">详情</span><br />
           <span v-if="billStatus == 1"
-           @click="$router.push({name: 'finance-billmanage-confirmbill', params: {id}})">确定运营账单</span>
+           @click="$router.push({name: 'finance-billmanage-confirmbill', params: {id}})">运营确定账单</span>
 
            <span v-if="billStatus == 3"
-           @click="$router.push({name: 'finance-billmanage-detail', params: {id, audit: 2}})">确定审核账单</span>
+           @click="$router.push({name: 'finance-billmanage-detail', params: {id, audit: 2}})">运营审核账单</span>
         </div>
       </template>
     </ListPage>
