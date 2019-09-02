dd78c835f504dcddd599eb1a4a1c9f1809170c3a
陈鹏宇 <1728252512@qq.com>
2019-09-02 19:58:47 +0800

    账户余额管理修改

--- src/api/withdrawal.ts
+++ src/api/withdrawal.ts
@@ -17,3 +17,23 @@ export async function withdrwaldetail(id: any) {
   const data = await get(`/finance/withdrawal-bills/${id}`)
   return data
 }
+
+/**
+ * @param  {any} query
+ * 提现的汇款
+ * http://yapi.aiads-dev.com/project/76/interface/api/5946
+ */
+export async function remittance(query: any) {
+  const data = await put(`/finance/withdrawal-bills/remittance`, query)
+  return data
+}
+
+/**
+ * @param  {any} query
+ * 提现的汇款
+ * http://yapi.aiads-dev.com/project/76/interface/api/5953
+ */
+export async function approval(query: any) {
+  const data = await put(`/finance/withdrawal-bills/approval`, query)
+  return data
+}
