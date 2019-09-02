008f7e7faa6ec9f7dbc509b662ad087e4563f55c
陈鹏宇 <1728252512@qq.com>
2019-09-02 10:20:34 +0800

    编辑系统热词修改

--- src/views/data/kol/edit.vue
+++ src/views/data/kol/edit.vue
@@ -317,6 +317,7 @@ export default class Main extends ViewBase {
         })
         this.logList = logList || []
         this.jyIndex = item.jyIndex
+        this.form.customTags = item.customTags ? item.customTags.join(';') : ''
         this.form.photo = item.photo ? [
           {
             url: item.photoUrl,
@@ -380,7 +381,7 @@ export default class Main extends ViewBase {
         customIndexPercent: Number(this.form.customIndexPercent),
         customJyIndex: Number(this.form.customJyIndex),
         photo: this.form.photo.map((it: any) => it.fileId).join(''),
-        customTags: this.form.customTags ? this.form.customTags.split(';') : [],
+        customTags: this.form.customTags ? this.form.customTags.split(';') : '',
         exts: this.form.exts.map((it: any) => {
           return {
             channelCode: it.channelCode,
