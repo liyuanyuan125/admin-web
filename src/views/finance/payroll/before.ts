1bdde1505ac79f3df883e083e49acc642fe21dba
陈鹏宇 <pengyu.chen@aiads.com>
2019-10-16 14:10:53 +0800

    请款单刷新 kol 关联关系 修改

--- src/views/finance/payroll/before.ts
+++ src/views/finance/payroll/before.ts
@@ -6,7 +6,7 @@ export const beforefetch: any =  [
   {
     name: 'cinemaId',
     defaultValue: 0,
-    type: remoteselect,
+    component: remoteselect,
     width: 128,
     placeholder: '影城名称'
   },
@@ -14,7 +14,7 @@ export const beforefetch: any =  [
   {
     name: 'code',
     defaultValue: '',
-    type: 'input',
+    input: true,
     width: 88,
     placeholder: '影城专资码'
   },
@@ -22,7 +22,7 @@ export const beforefetch: any =  [
   {
     name: 'resourceId',
     defaultValue: 0,
-    type: company,
+    component: company,
     width: 168,
     placeholder: '资源方公司名称'
   },
@@ -30,7 +30,7 @@ export const beforefetch: any =  [
   {
     name: 'resourceBillNo',
     defaultValue: '',
-    type: 'input',
+    input: true,
     width: 128,
     placeholder: '账单编号',
   },
@@ -38,7 +38,7 @@ export const beforefetch: any =  [
   {
     name: 'invoiceType',
     defaultValue: 0,
-    type: 'select',
+    select: true,
     width: 100,
     placeholder: '发票类型',
     enumKey: 'invoiceTypeCodeList'
@@ -47,7 +47,7 @@ export const beforefetch: any =  [
   {
     name: 'invoiceContent',
     defaultValue: '',
-    type: 'select',
+    select: true,
     width: 100,
     placeholder: '发票内容',
     enumKey: 'invoiceStatusList'
@@ -56,7 +56,7 @@ export const beforefetch: any =  [
   {
     name: 'time',
     defaultValue: '',
-    type: moouth,
+    component: moouth,
     width: 100,
     placeholder: '账单月份',
     dealParam(value: string) {
@@ -69,7 +69,7 @@ export const beforefetch: any =  [
   {
     name: 'dateRange',
     defaultValue: '',
-    type: 'dateRange',
+    dateRange: true,
     width: 200,
     placeholder: '对账时间',
     dealParam(value: string) {
@@ -114,18 +114,18 @@ export const beforenum: any = [
 export const beforcoulm: any = [
   { title: '账单编号', key: 'resourceBillNo', minWidth: 65 },
   { title: '影城名称', key: 'cinemaName', minWidth: 100 },
-  { title: '影城专资码', key: 'code', minWidth: 60, editor: 'deprecated' },
-  { title: '资源方名称', key: 'resourceName', minWidth: 60 },
+  { title: '影城专资码', key: 'code', minWidth: 70, deprecated: true },
+  { title: '资源方名称', key: 'resourceName', minWidth: 70 },
   {
     title: '账单月份',
     key: 'kolId',
     minWidth: 90,
     slot: 'month'
   },
-  { title: '对账完成时间', key: 'approvalTime', minWidth: 100, editor: 'dateTime' },
-  { title: '发票状态', key: 'invoiceStatus', minWidth: 100, editor: 'enum', enumKey: 'invoiceStatus' },
+  { title: '对账完成时间', key: 'approvalTime', minWidth: 100, dateTime: true },
+  { title: '发票状态', key: 'invoiceStatus', minWidth: 110, enum: 'invoiceStatusList' },
   { title: '发票类型', key: 'invoiceType', minWidth: 100, slot: 'invoiceType' },
-  { title: '发票内容', key: 'invoiceContentCode', minWidth: 65, editor: 'enum', enumKey: 'invoiceContentCodeCode'},
+  { title: '发票内容', key: 'invoiceContentCode', minWidth: 65, enum: 'invoiceContentCodeList'},
   { title: '账单金额', key: 'amount', minWidth: 90 },
   { title: '发票号', key: 'invoiceNo', minWidth: 100 },
   { title: '收款账户名', key: 'accountName', minWidth: 90 },
