0fc7261c92f96010d1d889d72b71657cabb0203a
张鹏 <peng.zhang@aiads.com>
2019-12-18 16:00:27 +0800

    完成影院对接 CMS

--- src/views/data/cinema/data.ts
+++ src/views/data/cinema/data.ts
@@ -1,4 +1,45 @@
 import { get } from '@/fn/ajax'
+import {
+  queryList as apiQueryList,
+  queryItem as apiQueryItem,
+  addItem as apiAddItem,
+  updateItem as apiUpdateItem
+} from '@/api/cinema'
+
+export async function fetchList(query: any) {
+  const { data } = await apiQueryList(query)
+  data.items = (data.items as any[] || []).map(item => {
+    // 纠正后端的类型与命名
+    item.tmsEnableStatus = item.tmsEnable === true ? 1 : 2
+    item.tmsBrand = item.tmsCode || ''
+    return item
+  })
+  return data
+}
+
+export async function queryItem(query: any = {}) {
+  const { data } = await apiQueryItem(query)
+  const item = data.item || (data.item = { tmsEnable: false })
+  // 纠正后端的类型与命名
+  item.tmsEnableStatus = item.tmsEnable === true ? 1 : 2
+  return data
+}
+
+const dealItem = (data: any) => {
+  data.tmsEnable = data.tmsEnableStatus == 1
+  delete data.tmsEnableStatus
+  return data
+}
+
+export async function addItem(item: any = {}) {
+  const data = dealItem(item)
+  return apiAddItem(data)
+}
+
+export async function updateItem(item: any = {}) {
+  const data = dealItem(item)
+  return apiUpdateItem(data)
+}
 
 interface CompanyListQuery {
   ids?: string
